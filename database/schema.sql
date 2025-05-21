90faa3fa8e80815e7835d969986ad777a82ec9d3
Kamil Napieraj <wrx55668@student.wroclaw.merito.pl>
2025-05-21 15:35:45 +0200

    change order of creating tables in db

--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,40 @@
+-- Tabele wymiarów
+
+CREATE TABLE Wymiar_Gmina (
+    id_gminy INT PRIMARY KEY,
+    kod_teryt VARCHAR(255),
+    nazwa_gminy VARCHAR(255),
+    typ_gminy VARCHAR(255),
+    powiat VARCHAR(255),
+    wojewodztwo VARCHAR(255)
+);
+
+CREATE TABLE Wymiar_Obwod (
+    id_obwodu VARCHAR(255) PRIMARY KEY,
+    numer_obwodu INT,
+    id_gminy INT,
+    adres VARCHAR(255),
+    przystosowany_dla_niepelnosprawnych VARCHAR(3),
+    typ_obwodu VARCHAR(255),
+    typ_obszaru VARCHAR(255),
+    FOREIGN KEY (id_gminy) REFERENCES Wymiar_Gmina(id_gminy)
+);
+
+CREATE TABLE Wymiar_Komitet (
+    id_komitetu INT PRIMARY KEY,
+    nazwa_komitetu VARCHAR(255),
+    skrot VARCHAR(255),
+    typ VARCHAR(255)
+);
+
+CREATE TABLE Wymiar_Czas (
+    id_czasu INT PRIMARY KEY,
+    rok INT,
+    miesiac INT,
+    dzien INT,
+    kwartal INT
+);
+
 
 -- Tabele faktów
 
@@ -25,41 +62,4 @@ CREATE TABLE Fakt_Statystyki_Obwodu (
     glosy_zaswiadczenie INT,
     FOREIGN KEY (id_obwodu) REFERENCES Wymiar_Obwod(id_obwodu),
     FOREIGN KEY (id_czasu) REFERENCES Wymiar_Czas(id_czasu)
-);
-
--- Tabele wymiarów
-
-CREATE TABLE Wymiar_Gmina (
-    id_gminy INT PRIMARY KEY,
-    kod_teryt VARCHAR(255),
-    nazwa_gminy VARCHAR(255),
-    typ_gminy VARCHAR(255),
-    powiat VARCHAR(255),
-    wojewodztwo VARCHAR(255)
-);
-
-CREATE TABLE Wymiar_Obwod (
-    id_obwodu VARCHAR(255) PRIMARY KEY,
-    numer_obwodu INT,
-    id_gminy INT,
-    adres VARCHAR(255),
-    przystosowany_dla_niepelnosprawnych BOOLEAN,
-    typ_obwodu VARCHAR(255),
-    typ_obszaru VARCHAR(255),
-    FOREIGN KEY (id_gminy) REFERENCES Wymiar_Gmina(id_gminy)
-);
-
-CREATE TABLE Wymiar_Komitet (
-    id_komitetu INT PRIMARY KEY,
-    nazwa_komitetu VARCHAR(255),
-    skrot VARCHAR(255),
-    typ VARCHAR(255)
-);
-
-CREATE TABLE Wymiar_Czas (
-    id_czasu INT PRIMARY KEY,
-    rok INT,
-    miesiac INT,
-    dzien INT,
-    kwartal INT
 );
