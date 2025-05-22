ea9da2b73a50909a1d0a68fdae4458aceda6b3b1
Kamil Napieraj <wrx55668@student.wroclaw.merito.pl>
2025-05-22 16:05:07 +0200

    rebuild remove action script

--- Scripts/Remove-AzureResources.ps1
+++ Scripts/Remove-AzureResources.ps1
@@ -1,23 +1,31 @@
-function Remove-AzureResources {
+function resourceGroupName {
     param (
         [Parameter(Mandatory = $true)]
         [string]
-        $resourceGroupName,
-
-        [Parameter(Mandatory = $true)]
-        [string]
-        $serverName,
-
-        [Parameter(Mandatory = $true)]
-        [string]
-        $sqlDBName
+        $resourceGroupName
     )
 
     Write-Host "INFO -- Usuwanie bazy danych '$sqlDBName' z serwera '$serverName'..."
     Remove-AzSqlDatabase -ResourceGroupName $resourceGroupName -ServerName $serverName -DatabaseName $sqlDBName -Force
+}
+
+function Remove-serverName {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $serverName
+    )
 
     Write-Host "INFO -- Usuwanie serwera SQL '$serverName'..."
     Remove-AzSqlServer -ResourceGroupName $resourceGroupName -ServerName $serverName -Force
+}
+
+function Remove-sqlDBName {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $sqlDBName
+    )
 
     Write-Host "INFO -- Usuwanie grupy zasobów '$resourceGroupName'..."
     Remove-AzResourceGroup -Name $resourceGroupName -Force -AsJob
