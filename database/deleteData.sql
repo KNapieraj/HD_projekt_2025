8b3cd6b8a21dc1e6a7773013f5a2bf9d5d8da848
Kamil Napieraj <wrx55668@student.wroclaw.merito.pl>
2025-06-10 16:11:14 +0200

    added deleteData.sql file

---
+++ database/deleteData.sql
@@ -0,0 +1,22 @@
+DELETE FROM Fakt_Wyniki_2015;
+DELETE FROM Fakt_Wyniki_2019;
+DELETE FROM Fakt_Wyniki_2023;
+
+DELETE FROM Fakt_Agregowane_Wyniki_2015;
+DELETE FROM Fakt_Agregowane_Wyniki_2019;
+DELETE FROM Fakt_Agregowane_Wyniki_2023;
+
+DELETE FROM Fakt_Agregowane_Gmina_2015;
+DELETE FROM Fakt_Agregowane_Gmina_2019;
+DELETE FROM Fakt_Agregowane_Gmina_2023;
+
+DELETE FROM Wymiar_Obwody_2015;
+DELETE FROM Wymiar_Obwody_2019;
+DELETE FROM Wymiar_Obwody_2023;
+
+DELETE FROM Wymiar_Gmina_2015;
+DELETE FROM Wymiar_Gmina_2019;
+DELETE FROM Wymiar_Gmina_2023;
+
+DELETE FROM Wymiar_Czas;
+DELETE FROM Wymiar_Komitet_Wyborczy;
